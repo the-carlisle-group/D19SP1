423fce637d1d02006a47b19efafffdcf8647a255
Norbert Jurkiewicz <norbert@carlislegroup.com>
2019-08-19 01:15:28 -0500

    Working remote execution

--- http/app.js
+++ http/app.js
@@ -1,2 +1,15 @@
-import "https://sdk.amazonaws.com/js/aws-sdk-2.510.0.min.js"
+window.onload = function() {
+    getValue();
+  };
 
+function getValue() {
+    let key = new Uint8Array(16);
+    crypto.getRandomValues(key);
+//    console.log(key);
+    //var data = string.fromCharCode(key);
+    var str = [...Array(10)].map(i=>(~~(Math.random()*36)).toString(36)).join('')
+    console.log(str);    
+    var s = document.getElementById('msg');
+
+    s.value = str;
+}
